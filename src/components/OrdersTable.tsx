02c7536266c97a8018652bdba4ac4d08efbadd1b
Avi <avi.leeker@gmail.com>
2024-03-26 22:42:32 +0200

    wip

--- src/components/OrdersTable.tsx
+++ src/components/OrdersTable.tsx
@@ -9,8 +9,8 @@ const OrdersTable: React.FC<MP_TABLE_DATA> = ({
   orders,
 }) => {
   const [selectAll, setSelectAll] = useState<boolean>(false);
-  const [isPopoverOpen, setIsPopoverOpen] = useState<boolean>(false);
   const [selectedRows, setSelectedRows] = useState<number[]>([]);
+  const [selectedPopover, setSelectedPopover] = useState<number[]>([]);
 
   const toggleSelectAll = () => {
     setSelectAll((prev) => !prev);
@@ -26,6 +26,13 @@ const OrdersTable: React.FC<MP_TABLE_DATA> = ({
       setSelectedRows(selectedRows.filter((row) => row !== rowIndex));
     } else {
       setSelectedRows([...selectedRows, rowIndex]);
+    }
+  };
+  const toggleSelectPop = (rowIndex: number) => {
+    if (selectedPopover.includes(rowIndex)) {
+      setSelectedPopover(selectedPopover.filter((row) => row !== rowIndex));
+    } else {
+      setSelectedPopover([...selectedPopover, rowIndex]);
     }
   };
   if (!orders) return null;
@@ -122,21 +129,154 @@ const OrdersTable: React.FC<MP_TABLE_DATA> = ({
                         <img src={order.service.fileIcon} alt='files icon' />
                       </span>
 
-                      <span onClick={() => setIsPopoverOpen(!isPopoverOpen)}>
+                      <span>
                         <span className='font-bold'>Estimate: </span>
                       </span>
                       {order.service.estimate}
                       <Popover
-                        isOpen={isPopoverOpen}
-                        positions={['top', 'bottom', 'left', 'right']}
+                        isOpen={selectedPopover.includes(index)}
+                        positions={['right']}
+                        padding={10}
+                        onClickOutside={() => toggleSelectPop(index)}
+                        content={() => (
+                          <div className='bg-white text-customGray p-4 rounded-2xl shadow-drop '>
+                            <h3 className='text-lg font-bold mb-2 pl-5 text-customOrange'>
+                              QUOTE DETAILS
+                            </h3>
+                            <ul className='list-disc pl-5'>
+                              <div className='mt-2 font-bold'>
+                                REF 1:{' '}
+                                <span className='font-normal '>
+                                  {order.service.details.ref1}
+                                </span>
+                              </div>
+                              <div className='mt-2 font-bold'>
+                                REF 2:{' '}
+                                <span className='font-normal '>
+                                  {order.service.details.ref2}
+                                </span>
+                              </div>
+                              <div className='mt-2 font-bold'>
+                                ZONE:{' '}
+                                <span className='font-normal '>
+                                  {order.service.details.zone}
+                                </span>
+                              </div>
+                              <div className='mt-2 font-bold'>
+                                USER:{' '}
+                                <span className='font-normal '>
+                                  {order.service.details.user}
+                                </span>
+                              </div>
+                              <div className='mt-2 font-bold'>
+                                SUBMITTED BY:{' '}
+                                <span className='font-normal '>
+                                  {order.service.details.submittedBy}
+                                </span>
+                              </div>
+                              <div className='mt-2 font-bold'>
+                                TRACKING:{' '}
+                                <span className='font-normal '>
+                                  {order.service.details.tracking}
+                                </span>
+                              </div>
+                              <div className='mt-2 font-bold'>
+                                SERVICE:{' '}
+                                <span className='font-normal '>
+                                  {order.service.details.service}
+                                </span>
+                              </div>
+                              <div className='mt-2 font-bold'>
+                                EST. DELIVERY DATE:{' '}
+                                <span className='font-normal '>
+                                  {order.service.details.estDelivery}
+                                </span>
+                              </div>
+                              <div className='mt-2 font-bold'>
+                                TRANSPORTATION:{' '}
+                                <span className='font-normal '>
+                                  {order.service.details.transFee}
+                                </span>
+                              </div>
+                              <div className='mt-2 font-bold'>
+                                CARRIER FEE(PEAK):{' '}
+                                <span className='font-normal '>
+                                  {order.service.details.carrierFee}
+                                </span>
+                              </div>
+                              <div className='mt-2 font-bold'>
+                                FUEL SURCHARGE:{' '}
+                                <span className='font-normal '>
+                                  {order.service.details.fuelSurcharge}
+                                </span>
+                              </div>
+                              <div className='mt-2 font-bold'>
+                                TOTAL:{' '}
+                                <span className='font-normal '>
+                                  {order.service.details.total}
+                                </span>
+                              </div>
+                            </ul>
+                          </div>
+                        )}
+                      >
+                        <span
+                          onClick={() => toggleSelectPop(index)}
+                          className='underline ml-2 cursor-pointer'
+                        >
+                          View Details
+                        </span>
+                      </Popover>
+                      {/* <Popover
+                        isOpen={selectedPopover.includes(index)}
+
+                        positions={['right']}
+                        
                         content={
-                          <div className='bg-white text-customGray'>
-                            {JSON.stringify(order.service.details)}
+                          <div className='bg-white text-customGray p-4'>
+                            <h3 className='text-lg font-bold mb-2'>
+                              Order Details
+                            </h3>
+                            <ul className='list-disc pl-5'>
+                              <li>Reference 1: {order.service.details.ref1}</li>
+                              <li>Reference 2: {order.service.details.ref2}</li>
+                              <li>Zone: {order.service.details.zone}</li>
+                              <li>User: {order.service.details.user}</li>
+                              <li>
+                                Submitted By:{' '}
+                                {order.service.details.submittedBy}
+                              </li>
+                              <li>
+                                Tracking: {order.service.details.tracking}
+                              </li>
+                              <li>Service: {order.service.details.service}</li>
+                              <li>
+                                Estimated Delivery:{' '}
+                                {order.service.details.estDelivery}
+                              </li>
+                              <li>
+                                Transportation Fee:{' '}
+                                {order.service.details.transFee}
+                              </li>
+                              <li>
+                                Carrier Fee: {order.service.details.carrierFee}
+                              </li>
+                              <li>
+                                Fuel Surcharge:{' '}
+                                {order.service.details.fuelSurcharge}
+                              </li>
+                              <li>Total: {order.service.details.total}</li>
+                            </ul>
                           </div>
                         }
                       >
-                        <span className='underline ml-2'>View Details</span>
-                      </Popover>
+                        <span
+                          onClick={() => toggleSelectPop(index)}
+                          className='underline ml-2 cursor-pointer'
+                        >
+                          View Details
+                        </span>
+                      </Popover> */}
                     </div>
                   ) : rowName === 'fromAddress' || rowName === 'toAddress' ? (
                     <>
