8ec35f762755d8d215f221fe902921075eab677f
Avi <avi.leeker@gmail.com>
2024-03-26 12:06:50 +0200

    wip

--- src/components/ui/Badge.tsx
+++ src/components/ui/Badge.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
-import { ColorClasses } from '../../types';
 import { colorClasses } from '../../consts';
 
 interface BadgeProps {
-  color: keyof ColorClasses;
+  color: keyof typeof colorClasses;
   text: string;
 }
 
 const Badge: React.FC<BadgeProps> = ({ color, text }) => {
   const badgeClasses = `inline-flex justify-center items-center align-center 
     rounded-md px-2 py-1 
-    text-xs font-medium ${colorClasses[color as keyof ColorClasses]}
-    ring-1 ring-inset ${
-      colorClasses[color as keyof ColorClasses]
-    } h-[38px] w-[96px]`;
+    text-xs font-medium ${colorClasses[color]}
+    ring-1 ring-inset ${colorClasses[color]} h-[38px] w-[96px]`;
 
   return <span className={badgeClasses}>{text}</span>;
 };
